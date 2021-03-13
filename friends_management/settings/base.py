a7850efb54553e984875f9c0efb208c26ebfdf0a
takayg <delight-ty.341026@ezweb.ne.jp>
2021-03-14 05:08:31 +0900

    modify settings

--- friends_management/settings/base.py
+++ friends_management/settings/base.py
@@ -12,14 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
 try:
     from .local import *
 except:
     from .production import *
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
