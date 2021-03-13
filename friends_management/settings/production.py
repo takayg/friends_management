b3bd721b715ec1681af3bb081d1b52aa47105d90
takayg <delight-ty.341026@ezweb.ne.jp>
2021-03-14 04:24:38 +0900

    modify manage.py 3

--- friends_management/settings/production.py
+++ friends_management/settings/production.py
@@ -1,4 +1,3 @@
-from .base import *
 import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
--- manage.py
+++ manage.py
@@ -7,10 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friends_management.settings')
-    try:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friends_management.settings.local')
-    except:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friends_management.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friends_management.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
