d51508dae81f7aa46895ee15ff817f05ebf407e7
takayg <delight-ty.341026@ezweb.ne.jp>
2021-03-14 04:48:24 +0900

    modify requirements.txt 2

--- friends_management/settings/production.py
+++ friends_management/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') # 環境変数に用意
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'friends_management',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('DB_USER'), # 環境変数に用意
+        'PASSWORD': os.environ.get('DB_PASSWORD'), # 環境変数に用意
         'HOST': 'localhost',
         'PORT': '',
     }
